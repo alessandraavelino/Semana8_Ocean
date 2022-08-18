841422cb2cb34afa00a1760ffd4a9cb03398b8de
Alessandra Avelino <80168478+alessandraavelino@users.noreply.github.com>
2022-08-17 21:59:47 -0300

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route Link, Routes } from 'react-router-dom';
 import './App.css';
 import Manatee from '../Manatee/Manatee';
 import Narwhal from '../Narwhal/Narwhal';
@@ -19,17 +19,11 @@ function App() {
             <li><Link to="/whale/blue">Blue Whale</Link></li>
           </ul>
         </nav>
-        <Switch>
-          <Route path="/manatee">
-            <Manatee />
-          </Route>
-          <Route path="/narwhal">
-            <Narwhal />
-          </Route>
-          <Route path="/whale">
-            <Whale />
-          </Route>
-        </Switch>
+        <Routes>
+          <Route path="manatee" element={<Manatee/>}></Route>
+          <Route path="narwhal" element={<Narwhal/>}></Route>
+          <Route path="whale" element={<Whale/>}></Route>
+        </Routes>   
       </BrowserRouter>
     </div>
   );
